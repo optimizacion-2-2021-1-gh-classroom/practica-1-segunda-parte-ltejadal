c61bfa7c4a194d57ea9be359e4a2c129767abc35
rafaelortegar <rafaelortega_itam@hotmail.com>
2021-03-25 16:09:39 -0600

    fix de imports

--- src/tests/aco_test.py
+++ src/tests/aco_test.py
@@ -1,13 +1,13 @@
 import numpy as np
 import random
 
-from ant_colony.aco_tsp import rand_dist_matrix
-from ant_colony.aco_tsp import create_dic_dist
-from ant_colony.aco_tsp import create_dic_dist_from_graph
-from ant_colony.aco_tsp import plot_graph
-from ant_colony.aco_tsp import init_ferom
-from ant_colony.aco_tsp import init_atrac
-from ant_colony.aco_tsp import atraccion_nodos
-from ant_colony.aco_tsp import hormiga_recorre
-from ant_colony.aco_tsp import graph_optim_path
-from ant_colony.aco_tsp import ant_colony
+from src.ant_colony.aco_tsp import rand_dist_matrix
+from src.ant_colony.aco_tsp import create_dic_dist
+from src.ant_colony.aco_tsp import create_dic_dist_from_graph
+from src.ant_colony.aco_tsp import plot_graph
+from src.ant_colony.aco_tsp import init_ferom
+from src.ant_colony.aco_tsp import init_atrac
+from src.ant_colony.aco_tsp import atraccion_nodos
+from src.ant_colony.aco_tsp import hormiga_recorre
+from src.ant_colony.aco_tsp import graph_optim_path
+from src.ant_colony.aco_tsp import ant_colony
