e1cc2b34d1bbd061e7d2c170560821dcff719c36
santibatte <santiagobattezzati@gmail.com>
2021-03-25 07:55:54 -0600

    minor changes to sum

--- src/ant_colony/sum.py
+++ src/ant_colony/sum.py
@@ -1,9 +1,9 @@
 #this file is only for testing that the package is working and that documentation is working
 
 def sum(x,y):
-	'''
-	args: x and y,
-	returns: x + y
-	'''
-
-    return x + y
+    '''
+    args: x and y,
+    returns: x + y
+    '''
+    z = x + y
+    return z
