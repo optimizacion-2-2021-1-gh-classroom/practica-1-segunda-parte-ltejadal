6e02c09a9201b631aeb7a9382def1f47a1d2c304
rafaelortegar <rafaelortega_itam@hotmail.com>
2021-03-25 19:45:54 -0600

    comentar imports

--- src/ant_colony/aco_test.py
+++ src/ant_colony/aco_test.py
@@ -1,13 +1,14 @@
 import numpy as np
 import random
+import aco_tsp
 
-from aco_tsp import rand_dist_matrix
-from aco_tsp import create_dic_dist
-from aco_tsp import create_dic_dist_from_graph
-from aco_tsp import plot_graph
-from aco_tsp import init_ferom
-from aco_tsp import init_atrac
-from aco_tsp import atraccion_nodos
-from aco_tsp import hormiga_recorre
-from aco_tsp import graph_optim_path
-from aco_tsp import ant_colony
+#from aco_tsp import rand_dist_matrix
+#from aco_tsp import create_dic_dist
+#from aco_tsp import create_dic_dist_from_graph
+#from aco_tsp import plot_graph
+#from aco_tsp import init_ferom
+#from aco_tsp import init_atrac
+#from aco_tsp import atraccion_nodos
+#from aco_tsp import hormiga_recorre
+#from aco_tsp import graph_optim_path
+#from aco_tsp import ant_colony
