0a06fe60c07df98a04bc8805dfc315eed3a1fa21
Sebastian Cadavid-Sanchez <s.cadavid1587@gmail.com>
2021-04-19 23:01:27 -0500

    Actualiza test

--- src/ant_colony/test_aco.py
+++ src/ant_colony/test_aco.py
@@ -4,15 +4,15 @@ import numpy as np
 import random
 from . import aco_tsp
 
-from .aco_tsp import rand_dist_matrix
+from .utils import rand_dist_matrix
 from .aco_tsp import create_dic_dist
 from .aco_tsp import create_dic_dist_from_graph
-from .aco_tsp import plot_graph
+from .utils import plot_graph
 from .aco_tsp import init_ferom
 from .aco_tsp import init_atrac
 from .aco_tsp import atraccion_nodos
 from .aco_tsp import hormiga_recorre
-from .aco_tsp import graph_optim_path
+from .utils import graph_optim_path
 from .aco_tsp import ant_colony
 
 # Auxiliares
