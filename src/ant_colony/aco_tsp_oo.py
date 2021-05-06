c3956f65a36b31fa72678a14412c06ef8f5b089a
Sebastian Cadavid-Sanchez <s.cadavid1587@gmail.com>
2021-05-06 18:44:45 -0500

    Agrega clase con pool de workers

--- src/ant_colony/aco_tsp_oo.py
+++ src/ant_colony/aco_tsp_oo.py
@@ -5,6 +5,147 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from .utils import *
 
+class colony_multiw():
+    """Clase que representa una colonia de hormigas que recorren
+    el grafo asignado para resolver el problema TSP.
+
+    Args:
+        G (networkx graph): Grafo con relaciones asociadas entre nodos
+        init_node (int): Nodo inicial del recorrido.
+        best_route (list, optional): Ruta con respecto a la cual se quiere mejorar.
+        best_dist ([type], optional): Distancia total del recorrido x_best.
+        n_ants (int, optional): Número de hormigas. Default es 2.
+        max_iter (int, optional): [description]. Default es 100.
+        alpha (int, optional): Factor de influencia de tau. Defaults to 1.
+        beta (int, optional): Factor de influencia de eta. Defaults to 5.
+        rho (float, optional): Tasa de evaporación de las feromonas. Defaults to .5.
+        verbose (int, optional): Imprime progreso del algoritmo cada K iteraciones. Defaults to 10.
+    """
+    def __init__(self, G, init_node,
+                 best_route = [],
+                 best_dist = float('inf'),
+                 n_ants=2,
+                 max_iter=10, 
+                 alpha=1, 
+                 beta=5, 
+                 rho=.5,
+                 n_workers = 1,
+                 verbose=10):
+        self.graph = G
+        self.A = None
+        self.init_node = init_node
+        self.best_route = best_route
+        self.best_dist = best_dist
+        self.lenghts = create_dic_dist_from_graph(self.graph)
+        self.n_ants = n_ants
+        self.max_iter = max_iter
+        self.alpha = alpha
+        self.beta = beta
+        self.rho = rho
+        self.n_workers = n_workers
+        self.ants_per_worker = assign_ants_threats(self.n_ants, self.n_workers)
+        self.tau = init_ferom(self.graph)
+        self.eta = init_atrac(self.graph, self.lenghts)
+        
+    def _update_pheromone_levels(self, route, dist_route):
+        """Actualiza el nivel de feromonas en las respectivas trayectorias
+        del grafo.
+
+        Args:
+            route (lst): Lista que incluye un recorrido por el grafo.
+            dist_route (float): Distancia de la ruta.
+        """
+        for i in range(1, len(route[:-1])):
+            self.tau[i-1][i] += 1/dist_route
+        self.tau[route[:-1][-1]][self.init_node]
+        
+    def _update_many_pheromone_levels(self, routes, distances):
+        """Actualiza los niveles de feromonas para diferentes rutas
+        recorridas por diferentes hormigas.
+
+        Args:
+            routes (lst of lst): Lista que contiene los recorridos 
+            realziados por diferentes hormigas.
+            distances (lst of floats): Lista con las distancias de
+            las rutas.
+        """
+        if (len(distances) == len(self.graph.nodes)):
+            for i in range(len(routes)):
+                self._update_pheromone_levels(routes[i], distances[i])
+                
+    def _evaporates_pheromone(self):
+        """Evapora los niveles de feromonas en todos los tramos del 
+        grafo.
+        """
+        for e in self.tau:
+            for v in self.tau:
+                self.tau[e][v] = (1-self.rho)*v
+                
+    def _n_ants_walk(self, n_ants):
+        ants_in_thread = [ant(self.graph) for i in range(n_ants)]
+        # solution for each ant
+        for a in ants_in_thread:
+            a.walk_over_graph(self.init_node, self.best_dist, self.A)
+        # tuple with sln for each ant
+        slns = [(a.route, a.r_len) for a in ants_in_thread]
+        return slns
+    
+    def _multiprocessing_bt(self, ants_per_threat, num_cpu):
+        with Pool(processes=num_cpu) as pool:
+            results = pool.starmap(self._n_ants_walk, ants_per_threat)
+        
+        return flatten_list_of_list(results)
+    
+    def _colony_run(self, A):
+        """La hormigas de la colonia realizan recorridos 
+        independientes simultáneamente.
+
+        Args:
+            A (dic): nivel de atracción de los nodos con respecto
+            a sus vecinos.
+        """
+        # multiprocessing
+        ants_journey = self._multiprocessing_bt(self.ants_per_worker, self.n_workers)
+        
+        # get paths and distances
+        routes, distances = zip(*ants_journey)
+        # updates pheromone levels
+        self._update_many_pheromone_levels(routes, distances)
+            
+        # best route
+        bst_idx = np.argmin(distances)
+        min_dist = distances[bst_idx]
+        bst_route = routes[bst_idx]
+        
+        # improves route if possible
+        if min_dist < self.best_dist:
+            self.best_dist = min_dist
+            self.best_route = bst_route
+            
+    def solve_tsp(self):
+        """Resuelve el problema TSP.
+        """
+        route = self.best_route
+        dist = self.best_dist
+        
+        for k in range(self.max_iter):
+            self.A = atraccion_nodos(self.graph,tau= self.tau, eta=self.eta, 
+                                alpha=self.alpha, beta=self.beta)
+            
+            if k>1:
+                self._evaporates_pheromone()
+                
+            # ants running across the graph
+            self._colony_run(self.A)
+            
+    def plot_route(self, plt_size=(12, 8)):
+        """Grafica la trayectoria encontrada por la colonia en el grafo.
+
+        Args:
+            plt_size (tuple, optional): Tamaño del gŕafico (ancho x altura). Defaults es (12, 8).
+        """
+        graph_optim_path(self.graph, self.best_route, self.best_dist, plt_size)
+
 class colony():
     """Clase que representa una colonia de hormigas que recorren
     el grafo asignado para resolver el problema TSP.
@@ -190,3 +331,5 @@ class ant():
             plt_size (tuple, optional): Tamaño del gŕafico (ancho x altura). Defaults es (12, 8).
         """
         graph_optim_path(self.graph, self.route, self.r_len, plt_size)
+
+
